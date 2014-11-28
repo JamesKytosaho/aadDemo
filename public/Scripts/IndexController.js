af6ece4cf0462c3da91fe7632a42c7514481e55b
JamesKytosaho <james_kytosaho@hotmail.com>
2014-11-28 15:32:33 +0100

    t

--- public/Scripts/IndexController.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('TodoSPA.Index', ['TodoSPA.Index.Controllers']);
-
-angular.module('TodoSPA.Index.Controllers', [])
-.controller('IndexController', ['$scope', 'adalAuthenticationService', function ($scope, adalAuthenticationService) {
-
-}]);
--- public/Scripts/UserDataController.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module('TodoSPA.User', ['TodoSPA.User.Controllers']);
-
-angular.module('TodoSPA.User.Controllers', [])
-    .controller('UserDataController', ['$scope', 'adalAuthenticationService', function ($scope, adalAuthenticationService) {
-
-
-}]);
