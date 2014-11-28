634d8e959f7745397d1a548f1f9c0e24e2be0c97
JamesKytosaho <james_kytosaho@hotmail.com>
2014-11-28 11:03:50 +0100

    t

--- public/Scripts/player.js
+++ public/Scripts/player.js
@@ -48,3 +48,41 @@ function placePlayerFrame(divId, event) {
 //<iframe style="width: 1000px; height: 600px;" src="http://localhost:3000/play/live/6559faa5cbee47149bbb751afd3cbd13/ccdd386908a94523afde69fab648f07c"></iframe>
 
 //https://mpsbroadband.sharepoint.com/sites/intranet/rd/_layouts/15/start.aspx#/SitePages/testAAD.aspx​
+
+
+'use strict';
+var app = angular.module('TodoSPA', ['ngRoute','AdalAngular']);
+// version 1
+app.config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalAuthenticationServiceProvider) {
+
+    $routeProvider.when("/Home", {
+        controller: "HomeController",
+        templateUrl: "/Views/Home.html"
+    }).when("/Iframe", {
+        controller: "IndexController",
+        templateUrl: "/Views/Iframe.html",
+        requireADLogin: true
+    }).when("/UserData", {
+        controller: "UserDataController",
+        templateUrl: "/Views/UserData.html"
+    }).otherwise({ redirectTo: "/Home" });
+
+    adalAuthenticationServiceProvider.init(
+        {
+            tenant: 'common',
+            clientId: 'a3e5d068-a35a-4596-a33f-8fdd95c7d641',
+            extraQueryParameter: ''
+        },
+        $httpProvider   // pass http provider to inject request interceptor to attach tokens
+    );
+    //5f388bf9-fc7f-4854-906c-d0819c9f951e
+    //client tenantId
+    //17c3954e-89ae-4242-9560-3a370147ba9e
+
+    /*
+     }).when("/TodoList", {
+     controller: "TodoListController",
+     templateUrl: "/Views/TodoList.html",
+     requireADLogin: true
+     */
+}]);
